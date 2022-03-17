44b82558b752ff5296235fdc4c2f0b09330b41ad
radoslaw <radoslawkazimierczuk@gmail.com>
2022-03-17 19:51:46 +0100

    added settings

--- js/main.js
+++ js/main.js
@@ -1,27 +1,64 @@
 let randomNum = Math.floor(Math.random()*10)+1;
 let win = false;
 let score = 20;
+const malin = document.getElementsByClassName('hidden')[0];
+const answernumber = document.getElementById('answernumber');
+const hint = document.getElementById('hint');
+const typednum = document.getElementById('typednum');
+const scorenum = document.getElementById('scorenum');
+const main = document.querySelector('.main');
+const colorbg = document.querySelector('.colorbg');
+const body = document.querySelector('body');
+
+colorbg.addEventListener('change',function(){
+  body.style.backgroundColor = colorbg.value;
+})
+
 
 function checkNumber() {
   if (!win) {
-  if(document.getElementById('typednum').value==randomNum)
+  if(typednum.value==randomNum)
   {
-    document.getElementById('number').innerHTML = document.getElementById('typednum').value
-    document.querySelector('body').style.backgroundColor='green';
-    document.getElementById('hint').innerText = 'Congratulations!'
+    answernumber.innerHTML = typednum.value
+    body.style.backgroundColor='green';
+    hint.innerText = 'Congratulations!'
     win = true;
   }
   else {
-    document.getElementById('hint').innerText = document.getElementById('typednum').value>randomNum?'Hint: Typed number is too high!':'Hint: Typed number is too low!';
+    hint.innerText = typednum.value>randomNum?'Hint: Typed number is too high!':'Hint: Typed number is too low!';
     score-=1;
-    document.getElementById('right').innerText = 'Score: '+ score;
+    document.getElementById('scorenum').innerText = 'Score: '+ score;
   }
 }
 else {
   if (confirm('Wanna play again?')) {
-    document.querySelector('body').style.backgroundColor='#303030';
-    document.getElementById('number').innerText = '?';
+    body.style.backgroundColor = colorbg.value;
+    answernumber.innerText = '?';
     win = false;
     randomNum = Math.floor(Math.random()*10)+1;
-    document.getElementById('hint').innerText = 'Hint';
+    hint.innerText = 'Hint';
+    score=20;
+    document.getElementById('scorenum').innerText = 'Score: '+score;
   }}}
+
+  document.getElementById('settings').addEventListener('click',showMalin)
+
+function showMalin(){
+  if (malin.classList.contains('hidden')) {
+  malin.classList.remove('hidden');
+  main.classList.add('blur');
+}}
+
+function hideMalin(){
+  if (!malin.classList.contains('hidden')) {
+    malin.classList.add('hidden');
+    main.classList.remove('blur');
+  }
+}
+
+
+
+  document.addEventListener('keyup', function(e){
+    if (e.keyCode == 27)
+    hideMalin();
+  })
